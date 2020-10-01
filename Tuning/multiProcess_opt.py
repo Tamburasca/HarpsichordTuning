f4a2908ca0bf35711fb67ce71b7658d78b15c2af
Tamburasca <rtimmermann@astro.uni-bonn.de>
2020-10-01 15:42:41 +0200

    minimzer with bounds and stepsize

--- Tuning/multiProcess_opt.py
+++ Tuning/multiProcess_opt.py
@@ -30,15 +30,18 @@ class MyBounds(object):
 class MyTakeStep(object):
 
    def __init__(self, initial):
-        self.s0 = initial[0] * 0.001  # stepsize for f0 within .1% at most
+        self.s0 = initial[0] * 0.002
         self.s1 = initial[1]
 
    def __call__(self, x):
+        # stepsize for f0 within .2% of the initial value i.e. equal to 3.5 cent
         x[0] += np.random.uniform(-self.s0, self.s0)
+        # stepsize for B is between factor .2 and 5 of the initial value unless B is zero in which case it is
+        # between 0 and INHARM/2
         if self.s1 == 0 or x[1] <= 0:
             x[1] = 10**np.random.uniform(-6, np.log10(INHARM/2)) - 1.e-6
         else:
-            x[1] *= 10**np.random.uniform(-1, 1)
+            x[1] *= 10**np.random.uniform(-0.7, .7)
         return x
 
 
@@ -71,9 +74,9 @@ class ThreadedOpt:
         processes = []
         logging.debug("Number of processes: " + str(self.num_threads))
 
-        for x in range(self.num_threads):  # Make the threads and start them off
+        for thread_id in range(self.num_threads):  # Make the threads and start them off
             p = Process(target=self.target_function,
-                        args=(queue, x,))
+                        args=(queue, thread_id,))
             processes.append(p)
             p.start()
 
@@ -122,6 +125,7 @@ class ThreadedOpt:
         https://scipy-lectures.org/advanced/mathematical_optimization/
         http://people.duke.edu/~ccc14/sta-663-2016/13_Optimization.html
         https://stackoverflow.com/questions/12781622/does-scipys-minimize-function-with-method-cobyla-accept-bounds
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.basinhopping.html
         """
         """
         result = minimize(self.chi_square,
@@ -129,12 +133,11 @@ class ThreadedOpt:
                           #bounds=bnds(self.a[thread_id]),
                           method="COBYLA",
                           constraints=cons(self.a[thread_id])
-                          #options = {'eps': 1}
                           )        
         result = shgo(self.chi_square,
-                          bounds=bnds(self.a[thread_id]),
-                          sampling_method='sobol'
-                          )
+                      bounds=bnds(self.a[thread_id]),
+                      sampling_method='sobol'
+                      )
         """
         minimizer_kwargs = {"method": "L-BFGS-B"}
         mybounds = MyBounds(f0=self.a[thread_id])
