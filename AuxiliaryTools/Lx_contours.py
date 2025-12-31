c5568a4db3aa163a92bb91cf7aa0b7561db13fb6
ralf <ralf.timmermann@gmx.de>
2025-12-31 21:14:49 +0100

    version 3.6.0 - see CHANGELOG.md

--- AuxiliaryTools/Lx_contours.py
+++ AuxiliaryTools/Lx_contours.py
@@ -1,10 +1,13 @@
 from numpy import log10, arange, meshgrid, stack, array, amin, amax, append
 import matplotlib.pyplot as plt
-from matplotlib import cm
+import matplotlib
 from Tuning.LxCostfunction import L1
 from Tuning.LxCostfunction import L2
 
-theCM = cm.get_cmap()
+matplotlib.pyplot.get_cmap('hsv')
+
+Minimizer = 'L1'  # 'L2'
+
 
 path = array([
     [204.92706698586852, 2.4512644719649316e-06],
@@ -25,7 +28,12 @@ founds = [(615.0377859618212, 3), (820.3618099250172, 4), (1026.4150561079955, 5
           (2875.4735685955284, 14), (3082.9471642076783, 15), (3289.285650695724, 16), (3493.6590575883847, 17),
           (3701.5213559994536, 18)]
 
-l1_min = L1(founds)
+if Minimizer == 'L1':
+    lx_min = L1(founds)
+elif Minimizer == 'L2':
+    lx_min = L2(founds)
+else:
+    raise ValueError("Minimizer must be 'L1'|'L2'")
 
 initial = [204.971, 4.504e-05, 0.019602835461558023]
 final = [205.138, 1.511e-05, 0.003178838204573313]
@@ -37,43 +45,46 @@ xm_u = max(initial[0], final[0], amax(path, axis=0)[0]) * 1.003
 xm_l = min(initial[0], final[0], amin(path, axis=0)[0]) * 0.997
 x = arange(xm_l,
            xm_u,
-           (xm_u - xm_l) / 500.)
+           (xm_u - xm_l) / 1000.)
 # lower, upper limit B
 # ym_l, ym_u = -5.0, -3.7
 ym_u = max(log10(initial[1]), log10(final[1]), log10(amax(path, axis=0))[1]) + .1
 ym_l = min(log10(initial[1]), log10(final[1]), log10(amin(path, axis=0))[1]) - .1
 y = arange(ym_l,
            ym_u,
-           (ym_u - ym_l) / 500.)
+           (ym_u - ym_l) / 1000.)
 xgrid, ygrid = meshgrid(x, y)
 xy = stack([xgrid, ygrid])
 
-res_l1 = list()
+res_lx = list()
 res_jac_f0 = list()
 res_jac_b = list()
 
 for y_data in y:
-    tmp_l1 = list()
+    tmp_lx = list()
     tmp_jac_f0 = list()
     tmp_jac_b = list()
 
     for x_data in x:
-        tmp = l1_min.l1_minimum_der([x_data, 10 ** y_data])
-        tmp_l1.append(tmp[0])
+        if Minimizer == 'L1':
+            tmp = lx_min.l1_minimum_der(array([x_data, 10 ** y_data]))
+        else: # L2
+            tmp = lx_min.l2_minimum_der(array([x_data, 10 ** y_data]))
+        tmp_lx.append(tmp[0])
         tmp_jac_f0.append(tmp[1][0])
         tmp_jac_b.append(tmp[1][1])
-    res_l1.append(tmp_l1)
+    res_lx.append(tmp_lx)
     res_jac_f0.append(tmp_jac_f0)
     res_jac_b.append(tmp_jac_b)
 
-res_l1 = array(res_l1)
+res_lx = array(res_lx)
 res_jac_f0 = array(res_jac_f0)
 res_jac_b = array(res_jac_b)
 
 plt.figure(1)
-cs = plt.contour(xgrid, ygrid, res_l1, 25, cmap='hsv')
+cs = plt.contour(xgrid, ygrid, res_lx, 25, cmap='hsv')
 plt.clabel(cs, inline=True, fontsize=10)
-plt.title("L1 cost function")
+plt.title(f"{Minimizer} cost function")
 plt.xlabel("f0/Hz")
 plt.ylabel("B (log)")
 plt.scatter(initial[0], log10(initial[1]), c='orange', label="initial")
@@ -88,7 +99,7 @@ plt.legend()
 plt.figure(2)
 cs = plt.contour(xgrid, ygrid, res_jac_f0, 30, cmap='hsv')
 plt.clabel(cs, inline=True, fontsize=10)
-plt.title("L1 cost function - Jacobian derived to f0")
+plt.title(f"{Minimizer} cost function - Jacobian derived to f0")
 plt.xlabel("f0/Hz")
 plt.ylabel("B (log)")
 plt.scatter(initial[0], log10(initial[1]), c='orange', label="initial")
@@ -103,7 +114,7 @@ plt.legend()
 plt.figure(3)
 cs = plt.contour(xgrid, ygrid, res_jac_b, 20, cmap='hsv')
 plt.clabel(cs, inline=True, fontsize=10)
-plt.title("L1 cost function - Jacobian derived to B")
+plt.title(f"{Minimizer} cost function - Jacobian derived to B")
 plt.xlabel("f0/Hz")
 plt.ylabel("B (log)")
 plt.scatter(initial[0], log10(initial[1]), c='orange', label="initial")
